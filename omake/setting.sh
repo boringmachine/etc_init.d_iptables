f4b4aa57bef252cb77ba01f58c8d3b3571304fed
root <root@localhost.localdomain>
2014-09-19 21:46:00 +0900

    fix

--- omake/setting.sh
+++ omake/setting.sh
@@ -1,32 +1,51 @@
 #!/bin/sh
 
+DATE=`date -d now +%Y%M%d%H%M%S`
+DIR=working_dir_${DATE}
+mkdir ${DIR}
+cd ${DIR}
+
 yes | yum update
 yes | yum install cron
-touch /etc/cron.daily/yum.cron
-echo '#!/bin/sh' >> /etc/cron.daily/yum.cron
-echo '' >> /etc/cron.daily/yum.cron
-echo '/usr/bin/yum -R 120 -e 0 -d 0 -y update yum' >> /etc/cron.daily/yum.cron
-echo '/usr/bin/yum -R 10 -e 0 -d 0 -y update' >> /etc/cron.daily/yum.cron
-chmod +x /etc/cron.daily/yum.cron
+if [ ! -f /etc/cron.daily/yum.cron ]; then
+    touch /etc/cron.daily/yum.cron
+    echo '#!/bin/sh' >> /etc/cron.daily/yum.cron
+    echo '' >> /etc/cron.daily/yum.cron
+    echo '/usr/bin/yum -R 120 -e 0 -d 0 -y update yum' >> /etc/cron.daily/yum.cron
+    echo '/usr/bin/yum -R 10 -e 0 -d 0 -y update' >> /etc/cron.daily/yum.cron
+    chmod +x /etc/cron.daily/yum.cron
+fi
 
 yes | yum install perl
 perl -p -i -e 's/PRELINKING=yes/PRELINKING=no/g' /etc/sysconfig/prelink
 /usr/sbin/prelink -ua
 yes | yum install aide
 /usr/sbin/aide --init
-cp /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz
+if [ ! -f /var/lib/aide/aide.db.gz ]; then
+    cp /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz
+fi
 /usr/sbin/aide --check
-echo "00 22 * * * root /usr/sbin/aide --check" >> /etc/crontab
+VAL=`cat /etc/crontab | grep aide`
+VAL=`echo ${#VAL}`
+if [ "${VAL}" == "0" ]; then
+    echo "00 22 * * * root /usr/sbin/aide --check" >> /etc/crontab
+fi
 
 yes | yum install clamav clamav-update
 perl -p -i -e 's/Example//g' /etc/freshclam.conf
 /usr/bin/freshclam
-touch /var/log/clamscan.log
-chown root:root /var/log/clamscan.log
-chmod 0600 /var/log/clamscan.log
+if [ ! -f /var/log/clamscan.log ]; then
+    touch /var/log/clamscan.log
+    chown root:root /var/log/clamscan.log
+    chmod 0600 /var/log/clamscan.log
+fi
 /usr/bin/clamscan -r -l /var/log/clamscan.log /home/
-echo "00 22 * * * root /usr/bin/freshclam" >> /etc/crontab
-echo "30 22 * * * root /usr/bin/clamscan -r -l /var/log/clamscan.log /home/" >> /etc/crontab
+VAL=`cat /etc/crontab | grep clam`
+VAL=`echo ${#VAL}`
+if [ "${VAL}" == "0" ]; then
+    echo "00 22 * * * root /usr/bin/freshclam" >> /etc/crontab
+    echo "30 22 * * * root /usr/bin/clamscan -r -l /var/log/clamscan.log /home/" >> /etc/crontab
+fi
 
 yes | yum install rsyslog
 echo "auth,user.*	/var/log/messages" >> /etc/rsyslog.conf
@@ -50,8 +69,12 @@ chmod 0600 /var/log/unused.log
 yes | yum install lynis
 /usr/bin/lynis --check-update
 /usr/bin/lynis -Q -c
-echo "00 22 * * * root /usr/bin/lynis --check-update" >> /etc/crontab
-echo "30 22 * * * root /usr/bin/lynis -Q -c" >> /etc/crontab
+VAL=`cat /etc/crontab | grep lynis`
+VAL=`echo ${#VAL}`
+if [ "${VAL}" == "0" ]; then
+    echo "00 22 * * * root /usr/bin/lynis --check-update" >> /etc/crontab
+    echo "30 22 * * * root /usr/bin/lynis -Q -c" >> /etc/crontab
+fi
 
 yes | yum install git
 git clone https://github.com/boringmachine/etc_init.d_iptables
@@ -72,14 +95,12 @@ chmod 644 /etc/sysctl.conf
 yes | yum install audit
 chkconfig auditd on
 perl -p -i -e 's/ROTATE/keep_logs/g' /etc/audit/auditd.conf
-echo "space_left_action = email" >> /etc/audit/auditd.conf
-echo "action_mail_acct = root" >> /etc/audit/auditd.conf
-echo "admin_space_left_action = halt" >> /etc/audit/auditd.conf
-cp /etc/audit/audit.rules /etc/audit/audit.rules.old
+yes | cp /etc/audit/audit.rules /etc/audit/audit.rules.old
 yes | cp audit.rules /etc/audit/audit.rules
 service auditd restart
 
 cd ..
 cd ..
-yes | rm etc_init.d_iptables -r
+cd ..
+yes | rm -r ${DIR}
 
