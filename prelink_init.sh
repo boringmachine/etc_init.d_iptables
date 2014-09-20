af0bd8eb6a290c1f476ac92b18a418258b274e7f
boringmachine <jasonss773@gmail.com>
2014-09-20 16:04:02 +0900

    update

--- prelink_init.sh
+++ prelink_init.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-yes | yum install perl
 cat /etc/sysconfig/prelink > /etc/sysconfig/prelink.old
 perl -p -i -e 's/PRELINKING=yes/PRELINKING=no/g' /etc/sysconfig/prelink
 /usr/sbin/prelink -ua
--- setting.sh
+++ setting.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 
 DATE=`date -d now +%Y%M%d%H%M%S`
-DIR=working_dir_${DATE}
+touch ${DATE}
+echo ${DATE} > ${DATE}
+DIR=`md5sum ${DATE} | awk  '{print $1;}'`
 mkdir ${DIR}
+rm ${DATE}
 cd ${DIR}
-yes | yum install git
+yes | yum install git perl
 git clone https://github.com/boringmachine/etc_init.d_iptables
 cd etc_init.d_iptables
 
-./yum_init.sh &
-./prelink_init.sh &
-./aide_init.sh &
-./clamav_init.sh &
-#./rsyslog_init.sh &
-./lynis_init.sh & 
-./iptables_init.sh &
-./sysctl_init.sh &
-./audit.sh &
+./yum_init.sh
+./prelink_init.sh
+./aide_init.sh
+./clamav_init.sh
+#./rsyslog_init.sh
+./lynis_init.sh
+./iptables_init.sh
+./sysctl_init.sh
+./audit.sh
 
 cd ..
 yes | rm -r ${DIR}
